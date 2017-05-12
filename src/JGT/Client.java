19ca7552b3cf9984d98426cad063c7c77df24308
Grzmot22 <grzmot22@gmail.com>
2017-05-12 21:00:49 +0100

    Merge branch 'singOutButton' of C:\Users\Ada\IdeaProjects\HotelBookingClient with conflicts.

--- src/JGT/Client.java
+++ src/JGT/Client.java
@@ -1,43 +1,13 @@
-package JGT; /**
- * Created by Błażej on 26.04.2017.
- */
-import java.io.IOException;
-import java.net.Socket;
+package JGT;
+import java.io.*;
+import java.net.*;
 
 public class Client
 {
-    private static JGT.GUI.Login login;
-
-    public void showClientLoginWindow(boolean b){
-
-        login.setVisible(b);
-
-    }
     public static void main(String[] args) throws IOException
     {
-        Socket socket = new Socket("localhost", 8189);
-         login = new JGT.GUI.Login(socket);
-
-        boolean quit = false;
+        Socket socket = new Socket("10.100.21.38", 8189);
+        JGT.GUI.Login login = new JGT.GUI.Login(socket);
 
     }
-        /*
-        while(!quit)
-        {
-            String sentence;
-            String modifiedSentence;
-            BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-
-            DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            sentence = inFromUser.readLine();
-            outToServer.writeBytes(sentence + '\n');
-            modifiedSentence = inFromServer.readLine();
-            System.out.println("FROM SERVER: " + modifiedSentence);
-            if(sentence.equals("quit"))
-                quit = true;
-        }
-        clientSocket.close();45
-        */
-    }
-
+}
