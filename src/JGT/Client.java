17f4427f73a649236a1c02a90673a5bc02154345
Grzmot22 <grzmot22@gmail.com>
2017-05-12 21:01:36 +0100

    Update

--- src/JGT/Client.java
+++ src/JGT/Client.java
@@ -1,13 +1,25 @@
 package JGT;
-import java.io.*;
-import java.net.*;
+import java.io.IOException;
+import java.net.Socket;
 
 public class Client
 {
+    private static JGT.GUI.Login login;
+
+    public void showClientLoginWindow(boolean b){
+
+        login.setVisible(b);
+
+    }
     public static void main(String[] args) throws IOException
     {
-        Socket socket = new Socket("10.100.21.38", 8189);
-        JGT.GUI.Login login = new JGT.GUI.Login(socket);
+        Socket socket = new Socket("localhost", 8189);
+//        Socket socket = new Socket("10.100.21.38", 8189);
+        login = new JGT.GUI.Login(socket);
+
+
+        boolean quit = false;
 
     }
 }
+
